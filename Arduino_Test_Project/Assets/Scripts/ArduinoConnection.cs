40ef939cf0d5822a1f8343bb7fa541d132f9fccd
Ace-03 <66760646+Ace-03@users.noreply.github.com>
2025-09-10 11:23:04 -0700

    Created project and coded controls

---
+++ Arduino_Test_Project/Assets/Scripts/ArduinoConnection.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System.IO.Ports;
+
+public class ArduinoConnection : MonoBehaviour
+{
+    SerialPort serial = new SerialPort("COM5", 115200);
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        serial.Open();
+        serial.ReadTimeout = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        string data = serial.ReadLine();
+        //Debug.Log("Recevied: '" + data + "'");
+        int value = int.Parse(data);
+
+        float normalized = (value - 512) / 512f;
+        float rotationSpeed = normalized * 100f;
+
+        //transform.rotation = Quaternion.Euler(0, value * 360 / 1023, 0);
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+        */
+
+        string data = serial.ReadLine().Trim();
+
+        string[] parts = data.Split(',');
+
+        if (parts.Length == 2)
+        {
+            int xValue = int.Parse(parts[0]);
+            int yValue = int.Parse(parts[1]);
+
+            float xNormalized = (xValue - 512) / 512f;
+            float yNormalized = (yValue - 512) / 512f;
+
+            float rotaionSpeed = 100;
+
+            float xRotation = -xNormalized * rotaionSpeed * Time.deltaTime;
+            float yRotation = yNormalized * rotaionSpeed * Time.deltaTime;
+
+            transform.Rotate(xRotation, yRotation, 0, Space.Self);
+        }
+
+        
+    }
+}
