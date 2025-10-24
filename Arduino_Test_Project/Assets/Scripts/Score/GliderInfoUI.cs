48c91e85460da01f2e2ae4cc38358dec628b0996
Donald Paladino <106718081+DUBBEP@users.noreply.github.com>
2025-10-24 12:50:19 -0700

    Added More UI

---
+++ Arduino_Test_Project/Assets/Scripts/Score/GliderInfoUI.cs
@@ -0,0 +1,32 @@
+using TMPro;
+using UnityEngine;
+
+public class GliderInfoUI : MonoBehaviour
+{
+    public TextMeshProUGUI score;
+    public TextMeshProUGUI height;
+    public TextMeshProUGUI speed;
+
+    public static GliderInfoUI instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this);
+    }
+
+    public void SetScoreText(string text)
+    {
+        score.text = "Score: " + text;
+    }
+    public void SetHeightText(string text)
+    {
+        height.text = "Height: " + text;
+    }
+    public void SetSpeedText(string text)
+    {
+        speed.text = "Speed: " + text;
+    }
+}
--- Arduino_Test_Project/Assets/Scripts/Score/ScoreTracker.cs
+++ Arduino_Test_Project/Assets/Scripts/Score/ScoreTracker.cs
@@ -3,14 +3,12 @@ using UnityEngine;
 
 public class ScoreTracker : MonoBehaviour
 {
-    public TextMeshProUGUI scoreText;
-    
     private GameObject player;
+    private Rigidbody playerRb;
     private string playerName = "Player";
 
+    float score = 0f;
     public static ScoreTracker Instance { get; private set; }
-
-    private float score = 0f;
 
     private void Awake()
     {
@@ -29,13 +27,19 @@ public class ScoreTracker : MonoBehaviour
     void Start()
     {
         player = GameObject.FindWithTag("Player");
+        playerRb = player.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         score = Mathf.Abs(player.transform.position.z);
-        scoreText.text = "Score: " + Mathf.FloorToInt(score).ToString();
+        string height = Mathf.FloorToInt(Mathf.Abs(player.transform.position.y)).ToString();
+        string speed = Mathf.FloorToInt(playerRb.linearVelocity.magnitude).ToString();
+
+        GliderInfoUI.instance.SetScoreText(Mathf.FloorToInt(score).ToString());
+        GliderInfoUI.instance.SetHeightText(height);
+        GliderInfoUI.instance.SetSpeedText(speed);
     }
 
     public void SetPlayerName(string name)
